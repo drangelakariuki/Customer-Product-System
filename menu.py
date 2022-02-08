1e5693d5e6615e35815960a63f6fd588c3b3706d
kuinikolaus@gmail.com <kuinikolaus@gmail.com>
2022-02-08 23:43:42 +0300

    better menu

--- menu.py
+++ menu.py
@@ -17,6 +17,14 @@ def menu_selection():
             break
         else:
             if selection in valid_selection:
+                if selection == '1':
+                    customer_display_menu()
+
+                elif selection == '2':
+                    product_display_menu()
+
+                elif selection == '3':
+                    purchase_display_menu()
                 loop = 'no'
 
             else:
@@ -25,26 +33,16 @@ def menu_selection():
     return selection
 
 
-def selection_calls():
-    selection = menu_selection()
-    if selection == '1':
-        customer_display_menu()
-
-    elif selection == '2':
-        product_display_menu()
-
-    elif selection == '3':
-        purchase_display_menu()
-
-
 if __name__ == '__main__':
-    selection_calls()
+    menu_selection()
     # ask admin if they want to do something else
-    do_more = input('Would you like to do anything else? Type yes or no. ')
-    if do_more.lower() == 'no':
-        print('Welcome again!')
-    elif do_more.lower() == 'yes':
-        selection_calls()
+    while True:
+        do_more = input('Would you like to do anything else? Type yes or no. ')
+        if do_more.lower() == 'no':
+            print('Welcome again!')
+            break
+        elif do_more.lower() == 'yes':
+            menu_selection()
 
 
 
